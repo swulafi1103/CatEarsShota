de006b8cc33fd7cd3092e7600ee31dad66f970cb
KURO-Games <kuro@kuro.games>
2019-05-17 15:12:06 +0900

    ペロー(プレイヤー)移動、アニメーション

--- CatEarsShota/Assets/Scripts/k-tamura/PlayerMoves.cs
+++ CatEarsShota/Assets/Scripts/k-tamura/PlayerMoves.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMoves : MonoBehaviour
 {
     [SerializeField]
     private float MoveSpeed = 0.1f;
     private Rigidbody2D rb2d;
-    private Vector2 AddVector = new Vector2(0.00f,0.00f);
+    private Vector2 AddVector = new Vector2(1.00f,1.00f);
+    private Animator anim;
     // Start is called before the first frame update
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     void Start()
     {
@@ -21,18 +24,32 @@ public class PlayerMoves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         //RightMove
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKey(KeyCode.D))
         {
-            AddVector.x *= MoveSpeed;
+            AddVector.x += MoveSpeed;
             rb2d.AddForce(AddVector);
+            anim.SetBool("SetWaitAnimator", false);
+            anim.SetBool("SetWalkAnimator", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            AddVector.x += -1 * MoveSpeed;
+            rb2d.AddForce(AddVector);
+            anim.SetBool("SetWaitAnimator", false);
+            anim.SetBool("SetWalkAnimator", true);
+        }else
+        {
+            AddVector.x = 1;
+            AddVector.y = 1;
+            anim.SetBool("SetWaitAnimator", true);
+            anim.SetBool("SetWalkAnimator", false);
         }
         //LeftMove
-        if (Input.GetKeyDown(KeyCode.B))
-        {
-            AddVector.x *= -MoveSpeed;
-            rb2d.AddForce(AddVector*-1);
-        }
-        AddVector = Vector2.zero;
+        
+       
+        
     }
 }
