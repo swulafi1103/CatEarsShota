e54dfe7c1c47eb721f1fb9afe12f8bdb1d877c1c
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-05-17 14:20:35 +0900

    UIの描画周りの調整

--- CatEarsShota/Assets/Scripts/izu/MiniGameManager.cs
+++ CatEarsShota/Assets/Scripts/izu/MiniGameManager.cs
@@ -2,16 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MiniGameManager : MonoBehaviour
 {
-    //[SerializeField, Range(0.1f, 15f)]
-    //private float timeLimit = 10f;
+    [SerializeField, Range(0.1f, 15f)]
+    private float timeLimit = 10f;          //  ミニゲームの制限時間
+    private const int MISTAKECOUNT = 4;     //  失敗カウント
+    private static readonly KeyCode[] USEKEY = { KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.LeftArrow, KeyCode.RightArrow };    //  ミニゲームに使うキー配列
+    private KeyCode[] qustionKey;
+
     [SerializeField]
-    private KeyCode[] targetKeys = new KeyCode[6];
+    private Canvas miniGameCanvas;
+    //[SerializeField]
+    //private Image[] countDownImage;
 
     void Start()
     {
+        StartCoroutine(CountDown());
         GenerateRandomNums(5);
     }
 
@@ -21,6 +29,25 @@ public class MiniGameManager : MonoBehaviour
         CheckTypingKey();
     }
 
+    /// <summary>
+    /// ミニゲームの開始時に表示
+    /// </summary>
+    IEnumerator CountDown()
+    {
+        Text txtObj = miniGameCanvas.transform.GetChild(0).gameObject.GetComponent<Text>();
+        yield return new WaitForSeconds(1f);
+        txtObj.enabled = true;
+        txtObj.text = "3";
+        yield return new WaitForSeconds(0.5f);
+        txtObj.text = "2";
+        yield return new WaitForSeconds(0.5f);
+        txtObj.text = "1";
+        yield return new WaitForSeconds(0.5f);
+        txtObj.text = "START";
+        yield return new WaitForSeconds(0.5f);
+        txtObj.enabled = false;
+        yield break;
+    }
 
     /// <summary>
     /// 同じ数字が続かないランダムな配列の生成
@@ -42,36 +69,57 @@ public class MiniGameManager : MonoBehaviour
             {
                 while (tmp == randNum)
                 {
-                    tmp = UnityEngine.Random.Range(0, size + 1);
+                    randNum = UnityEngine.Random.Range(0, size + 1);
                 }
+                tmp = randNum;
                 randomNums[i] = randNum;
             }
         }
+        CreateQuesAry(randomNums);
         DrawUI(randomNums);
     }
 
+    /// <summary>
+    /// 問題となるのキー配列を生成
+    /// </summary>
+    /// <param name="randomNums"></param>
+    private void CreateQuesAry(int[] randomNums)
+    {
+        qustionKey = new KeyCode[randomNums.Length];
+        for (int i = 0; i < randomNums.Length; i++)
+        {
+            qustionKey[i] = USEKEY[randomNums[i]];
+        }
+    }
 
     /// <summary>
     /// UIの描画
     /// </summary>
-    /// <param name="rands"></param>
-    private void DrawUI(int[] rands)
+    /// <param name="randomNums"></param>
+    private void DrawUI(int[] randomNums)
     {
-        
+        for (int i = 0; i < randomNums.Length ; i++)
+        {            
+            Debug.Log("Num = " + randomNums[i]);
+        }
     }
-
 
     /// <summary>
     /// タイピングしたキーの判定
     /// </summary>
     void CheckTypingKey()
     {
-        for (int i = 0; i < targetKeys.Length; i++)
+        if (Input.anyKeyDown)
         {
-            if (Input.GetKeyDown(targetKeys[i]))
+            for (int i = 0; i < USEKEY.Length; i++)
             {
-                Debug.Log(targetKeys[i]);
+                if (Input.GetKeyDown(USEKEY[i]))
+                {
+                    Debug.Log(USEKEY[i]);
+                    return;
+                }
             }
+            Debug.Log("使わないキー");
         }
     }
 
