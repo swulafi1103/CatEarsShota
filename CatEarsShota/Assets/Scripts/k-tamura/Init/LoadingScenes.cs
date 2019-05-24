96c569aacc9da06efe9e3e419591b394ecc89ac2
KURO-Games <kuro@kuro.games>
2019-05-24 14:04:42 +0900

    シーンフェードシステム（singleton）

---
+++ CatEarsShota/Assets/Scripts/k-tamura/Init/LoadingScenes.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class LoadingScenes : MonoBehaviour {
+	/*
+	[SerializeField]
+	private AsyncOperation async;
+	public GameObject LoadingUI;
+	public Slider Slider;
+	public void LoadNextScene(){
+		LoadingUI.SetActive(true);
+		StartCoroutine(LoadScene());
+	}
+
+	IEnumerator LoadScene(){
+		async = SceneManager.LoadSceneAsync("");
+		while(!async.isDone){
+			Slider.value = async.progress;
+			yield return null;
+		}
+	}
+	*/
+
+	[SerializeField]
+	private float FadeInTime;
+
+	private Image image;
+	private void Start()
+	{
+		image = transform.Find("Panel").GetComponent<Image>();
+		FadeInTime = 1f * FadeInTime / 10f;
+		StartCoroutine("FadeIn");
+
+	}
+	IEnumerator FadeIn(){
+		for (var i = 1f; i >= 0;i-=0.1f){
+			image.color = new Color(0f, 0f, 0f, i);
+			yield return new WaitForSeconds(FadeInTime);
+		}
+	}
+
+}
