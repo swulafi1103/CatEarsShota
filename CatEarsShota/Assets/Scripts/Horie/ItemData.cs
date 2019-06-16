69370d7636e2866c3ebd3ea821cdfd86c8813584
Horie_miku <17d3170023hm@edu.tech.ac.jp>
2019-06-17 00:21:24 +0900

    単体アイテム情報格納用ScriptableObject作成

---
+++ CatEarsShota/Assets/Scripts/Horie/ItemData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New Item",menuName ="CreateItem")]
+public class ItemData : ScriptableObject 
+{
+    public enum ItemType {
+        Nomal=0,
+        Report,
+        Pants
+    };
+
+    [SerializeField]
+    private ItemType type;
+
+    public ItemType GetItemType {
+        get { return type; }
+    }
+
+    [SerializeField]
+    private int itemNum;
+
+    public int GetItemNum {
+        get { return itemNum; }
+    }
+
+    [SerializeField]
+    private string itemName;
+
+    public string GetItemName {
+        get { return itemName; }
+    }
+
+    [SerializeField]
+    private Sprite itemSprite;
+
+    public Sprite GetItemSprite {
+        get { return itemSprite; }
+    }
+
+    /// <summary>
+    /// レポートのみ表示される文章を書く
+    /// (その他はitemManagerの方で取得しないようにする)
+    /// </summary>
+    [SerializeField]
+    [Multiline]
+    private string reportText;
+
+    public string GetReportText {
+        get { return reportText; }
+    }
+
+}
