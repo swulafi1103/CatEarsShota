1fbb1e29cd1ae6de59bd1eecd2eee2edce107500
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-06-17 15:40:37 +0900

    坂を減らしたMap１画像に変更

---
+++ CatEarsShota/Assets/Scripts/InputManager.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class InputManager : MonoBehaviour
+{
+    protected static readonly string[] findTags =
+    {
+        "InputManager",
+    };
+
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+
+                Type type = typeof(InputManager);
+
+                foreach (var tag in findTags)
+                {
+                    GameObject[] objs = GameObject.FindGameObjectsWithTag(tag);
+
+                    for (int j = 0; j < objs.Length; j++)
+                    {
+                        instance = (InputManager)objs[j].GetComponent(type);
+                        if (instance != null)
+                            return instance;
+                    }
+                }
+
+                Debug.LogWarning(string.Format("{0} is not found", type.Name));
+            }
+
+            return instance;
+        }
+    }
+
+    void Awake()
+    {
+        CheckInstance();
+    }
+
+    private bool CheckInstance()
+    {
+        if (instance == null)
+        {
+            instance = (InputManager)this;
+            DontDestroyOnLoad(gameObject);
+            return true;
+        }
+        else if (Instance == this)
+        {
+            return true;
+        }
+
+        Destroy(this);
+        return false;
+    }
+
+    /* -- Horizontal入力 --------------------------------------------------------------------------- */
+    private float moveKey = 0;
+    public float MoveKey
+    {
+        get { return moveKey; }
+    }
+
+    /* -- Jump入力 --------------------------------------------------------------------------------- */
+    private int jumpKey = 0;
+    public int JumpKey
+    {
+        get { return jumpKey; }
+    }
+
+    void Update()
+    {
+        // 移動
+        moveKey = Input.GetAxisRaw("Horizontal");
+        // ジャンプ
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpKey = 1;
+        }
+        else if (Input.GetButton("Jump"))
+        {
+            jumpKey = 2;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            jumpKey = 0;
+        }
+    }
+}
--- CatEarsShota/Assets/Scripts/izu/PerraultMove.cs
+++ CatEarsShota/Assets/Scripts/izu/PerraultMove.cs
@@ -12,7 +12,23 @@ public class PerraultMove : MonoBehaviour
      * ステータス          (HP・ATK ここに書くべきではない気がする)
      */
 
-    
+    [SerializeField] private Rigidbody2D rb;
+    private Vector2 localScale;
+    private bool isGrounded = true;
+    private bool isJumping = false;
+    private bool isJumpingCheck = true;
+    private float jumpTimeCounter;
+    private float jumpTime = 0.35f;
+    private float _jumpPower;
+    [SerializeField]
+    private LayerMask platformLayer;
+
+    [SerializeField]
+    ContactFilter2D filter2d;
+
+    InputManager inputManager;
+    PlayerManager playerManager;
+
     [SerializeField]
     private float moveSpeed;            //  移動速度
     [SerializeField]
@@ -22,13 +38,25 @@ public class PerraultMove : MonoBehaviour
     private float _horizontalInput;
     private float _verticalInput;
 
+    void Awake()
+    {
+        localScale = transform.localScale;
+        jumpTimeCounter = jumpTime;
+    }
+
+
     void Start()
     {
+        playerManager = PlayerManager.Instance;
+        inputManager = InputManager.Instance;
         _rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        isGrounded = Physics2D.Linecast(transform.position - transform.up * 0.4f, transform.position - transform.up * 0.6f, platformLayer);
+        //isGrounded = _rb.IsTouching(filter2d);
+        Debug.Log("着地：" + isGrounded);
         _horizontalInput = Input.GetAxis("Horizontal");
         _verticalInput = Input.GetAxis("Vertical");
     }
@@ -40,5 +68,58 @@ public class PerraultMove : MonoBehaviour
         //moveVector.y = moveSpeed * _verticalInput;
 
         _rb.AddForce(moveForceMultiplier * (moveVector - _rb.velocity));
+
+        if (inputManager.MoveKey != 0)
+        {
+            // 向きを変える
+            localScale.x = inputManager.MoveKey;
+            transform.localScale = localScale;
+        }
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(inputManager.MoveKey * playerManager.MoveSpeed, rb.velocity.y);
+
+            if (isJumpingCheck && inputManager.JumpKey != 0)
+            {
+                jumpTimeCounter = jumpTime;
+                isJumpingCheck = false;
+                isJumping = true;
+                _jumpPower = playerManager.JumpPower;
+            }
+        }
+        else
+        {
+            if (inputManager.JumpKey == 0)
+            {
+                isJumping = false;
+            }
+            if (!isJumping)
+            {
+                rb.velocity = new Vector2(inputManager.MoveKey * playerManager.JumpMoveSpeed, Physics.gravity.y * playerManager.GravityRate);
+            }
+        }
+
+        if (isJumping)
+        {
+            jumpTimeCounter -= Time.deltaTime;
+
+            if (inputManager.JumpKey == 2)
+            {
+                _jumpPower -= 0.2f;
+                rb.velocity = new Vector2(inputManager.MoveKey * playerManager.JumpMoveSpeed, 1 * _jumpPower);
+            }
+            if (jumpTimeCounter < 0)
+            {
+                isJumping = false;
+            }
+        }
+
+        if (inputManager.JumpKey == 0)
+        {
+            isJumpingCheck = true;
+        }
     }
+
+
 }
