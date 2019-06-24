aa630afe22e584e1f670029a3f5a51fdbe4b1eb9
Horie_miku <17d3170023hm@edu.tech.ac.jp>
2019-06-24 12:14:16 +0900

    移動→ジャンプ→二段ジャンプのチュートリアル作成

---
+++ CatEarsShota/Assets/Scripts/Horie/Tutorial/IconTutorial.cs
@@ -0,0 +1,188 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconTutorial : MonoBehaviour
+{
+    [SerializeField]
+    List<Sprite> Icons = new List<Sprite>();
+    [SerializeField]
+    Image IconImage;
+
+    int AnimFrame = 30;
+
+    Rigidbody2D PlayerRid;
+
+    bool ActAnim = false;
+
+    float moveVec = 0;
+    [SerializeField]
+    float moveRange = 1000;
+
+    bool FirstJump = false;
+
+    public enum IconNum {
+        Move = 0,
+        Jump,
+        HiJump,
+        Item,
+        ItemUI,
+        None
+    }
+
+    private IconNum NowNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NowNum = IconNum.None;
+        ActAnim = false;
+        FirstJump = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (NowNum) {
+            case IconNum.Move:
+                CheckMove();
+                break;
+            case IconNum.Jump:
+                CheckJump();
+                break;
+            case IconNum.HiJump:
+                CheckHiJump();
+                break;
+            case IconNum.Item:
+                break;
+            case IconNum.ItemUI:
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 判定にRigidBody必須なチュートリアル表示
+    /// 移動
+    /// </summary>
+    public void MoveTuto(Rigidbody2D rid) {
+        PlayerRid = rid;
+        NowNum = IconNum.Move;
+        StartCoroutine(StartAnim());
+    }
+
+    /// <summary>
+    /// RigidBody不必要なチュートリアル表示
+    /// </summary>
+    public void IconTuto(IconNum num) {
+        NowNum = num;
+        StartCoroutine(StartAnim());
+    }
+
+    /// <summary>
+    /// 移動判定
+    /// </summary>
+    void CheckMove() {
+        moveVec += Mathf.Abs(PlayerRid.velocity.x);
+        if (moveVec >= moveRange) {
+            StartCoroutine(AnimSet(IconNum.Jump));
+        }
+    }
+
+    /// <summary>
+    /// ジャンプ判定
+    /// </summary>
+    void CheckJump() {
+        if (!Input.GetKeyDown(KeyCode.Space)) return;
+        StartCoroutine(AnimSet(IconNum.HiJump));
+    }
+
+    /// <summary>
+    /// 二段ジャンプ判定
+    /// </summary>
+    void CheckHiJump() {
+
+        if (FirstJump) {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                StartCoroutine(EndAnim());
+                FirstJump = false;
+            }else if (Mathf.Abs(PlayerRid.velocity.y) == 0) {
+                FirstJump = false;
+                return;
+            }
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                FirstJump = true;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 非表示→次表示一連操作
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+    IEnumerator AnimSet(IconNum num) {
+        var col = StartCoroutine(EndAnim());
+        yield return col;
+        IconTuto(num);
+    }
+
+    /// <summary>
+    /// 表示アニメーション開始
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator StartAnim() {
+        if (ActAnim) {
+            yield break;
+        }
+        ActAnim = true;
+
+        int SPNum = (int)NowNum;
+        if (SPNum >= Icons.Count) {
+            Debug.Log("NULL NUM:" + SPNum);
+            yield break;
+        }
+        IconImage.enabled = false;
+
+        Color color = Color.white;
+        color.a = 0;
+        IconImage.color = color;
+        IconImage.sprite = Icons[SPNum];
+        IconImage.enabled = true;
+        for (int i = 0; i <= AnimFrame; i++) {
+            float a = (float)i / AnimFrame;
+            color.a = a;
+            IconImage.color = color;
+            yield return null;
+        }
+        ActAnim = false;
+    }
+
+    /// <summary>
+    /// 終了アニメーション開始
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator EndAnim() {
+        if (ActAnim) {
+            yield break;
+        }
+        ActAnim = true;
+
+        Color color = Color.white;
+        color.a = 1;
+        IconImage.color = color;
+
+        for(int i = AnimFrame; i >= 0; i--) {
+            float a = (float)i / AnimFrame;
+            color.a = a;
+            IconImage.color = color;
+            yield return null;
+        }
+        NowNum = IconNum.None;
+        ActAnim = false;
+    }
+}
--- CatEarsShota/Assets/Scripts/Horie/Tutorial/TutorialContriller.cs
+++ CatEarsShota/Assets/Scripts/Horie/Tutorial/TutorialContriller.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class TutorialContriller : MonoBehaviour
 {
+    GameObject PerraultObj;
+    GameObject FranObj;
+    IconTutorial iconTutorial;
+
+    static TutorialContriller instance = null;
+
+    public static TutorialContriller Instance {
+        get { return instance; }
+    }
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetData();
+        MoveTutorial();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SetData() {
+        PlayerMoves[] objs = FindObjectsOfType<PlayerMoves>();
+        foreach(PlayerMoves player in objs) {
+            if (player.gameObject.name == "Perrault") {
+                PerraultObj = player.gameObject;
+            }
+            if (player.gameObject.name == "Fran") {
+                FranObj = player.gameObject;
+            }
+        }
+
+        iconTutorial = GetComponent<IconTutorial>();
+    }
+
+    /// <summary>
+    /// 移動、ジャンプ、二段ジャンプ
+    /// </summary>
+    public void MoveTutorial() {
+        Rigidbody2D p = PerraultObj.GetComponent<Rigidbody2D>();
+        iconTutorial.MoveTuto(p);
     }
 }
