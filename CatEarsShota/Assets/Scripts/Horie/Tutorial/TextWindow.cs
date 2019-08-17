16e78dca52badd82e12b5adfb33e5ba2b8413b49
Horie_miku <17d3170023hm@edu.tech.ac.jp>
2019-08-17 14:37:19 +0900

    テキストウィンドウ表示

---
+++ CatEarsShota/Assets/Scripts/Horie/Tutorial/TextWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextWindow : MonoBehaviour
+{
+    [SerializeField]
+    Sprite[] TextImages;
+
+    Image Window;
+
+    float hiddenTime = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Window = GetComponent<Image>();
+        Window.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetWindow(int num) {
+        Window.sprite = TextImages[num];
+        Window.SetNativeSize();
+        Window.enabled = true;
+        StartCoroutine(HiddenWindow());
+    }
+
+    IEnumerator HiddenWindow() {
+
+        yield return new WaitForSeconds(hiddenTime);
+
+        Window.enabled = false;
+
+        yield break;
+    }
+}
