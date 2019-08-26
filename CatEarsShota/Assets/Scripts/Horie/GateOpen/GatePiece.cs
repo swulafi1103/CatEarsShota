e82b34bf703443201053df87447c221f6240c880
mikuhorie <17d3170023hm@edu.tech.ac.jp>
2019-08-26 14:27:07 +0900

    ゲートのアニメーション追加

---
+++ CatEarsShota/Assets/Scripts/Horie/GateOpen/GatePiece.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatePiece : MonoBehaviour
+{
+    [SerializeField]
+    BoxCollider2D[] Colliders;
+    
+
+    public void OnEnableCollider(int num)
+    {
+        Colliders[num].enabled = false;
+    }
+}
