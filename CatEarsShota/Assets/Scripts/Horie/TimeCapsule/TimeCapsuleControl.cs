a79b7c3da4095ba500400009b44c8249e67fc808
Horie_miku <17d3170023hm@edu.tech.ac.jp>
2019-09-04 23:39:33 +0900

    タイムカプセル機能仮組

---
+++ CatEarsShota/Assets/Scripts/Horie/TimeCapsule/TimeCapsuleControl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCapsuleControl : MonoBehaviour
+{
+    private static TimeCapsuleControl instance;
+    public static TimeCapsuleControl Instance {
+        get { return instance; }
+    }
+
+    private void Awake() {
+        instance = this;
+    }
+
+    ItemManager.ItemNum[] list = new ItemManager.ItemNum[2] {
+        ItemManager.ItemNum.CardKey,
+        ItemManager.ItemNum.BookMark_Past
+    };
+
+    [SerializeField]
+    SpriteRenderer pastTimeCapsule;
+
+    [SerializeField]
+    SpriteRenderer nowTimeCapsule;
+
+    [SerializeField]
+    Sprite nowOnItemSprite;
+
+    [SerializeField]
+    Sprite pastOnItemSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 過去で埋めるためにアイテム欄表示する
+    /// </summary>
+    /// <param name="num"></param>
+    public void SetUI(int num) {
+        if (num == 0) {
+            ItemManager.Instance.SelectEvent(list[num], SetKey);
+        }
+        else {
+            ItemManager.Instance.SelectEvent(list[num], SetBookmark);
+        }
+    }
+    
+    void SetKey() {
+        setItem(0);
+    }
+
+    void SetBookmark() {
+        setItem(1);
+    }
+
+    /// <summary>
+    /// アイテム選択された
+    /// </summary>
+    /// <param name="num"></param>
+    void setItem(int num) {
+        //画像変更処理
+        pastTimeCapsule.sprite = pastOnItemSprite;
+        nowTimeCapsule.sprite = nowOnItemSprite;
+
+        //フラグ処理
+    }
+}
