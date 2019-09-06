130b23dbd9a9dbb83b2c022b52cf80e0a99bf763
mametaro117 <16c1020003ik@edu.tech.ac.jp>
2019-09-06 10:29:51 +0900

    リスポーン処理の追加

---
+++ CatEarsShota/Assets/Scripts/izu/Event/Respawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField]
+    private Transform respawnPos;
+
+    void CallCortineFallDead()
+    {
+        StartCoroutine(FallDeadCor());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.transform.position.y > gameObject.transform.position.y)
+            CallCortineFallDead();
+    }
+
+    private IEnumerator FallDeadCor()
+    {
+        Fade.Instance.StartFade(0.5f, Color.black);
+        while (!Fade.Instance.Fading == false)
+            yield return null;
+        PlayerManager.Instance.Pero.transform.position = respawnPos.position;
+        Fade.Instance.ClearFade(0.5f, Color.clear);
+        TutorialContriller.Instance.SetTextWindow(0);
+        yield break;
+    }
+
+
+}
