ea02d3d6f821587385f770ec1b8e062eb27388b8
mikuhorie <17d3170023hm@edu.tech.ac.jp>
2019-09-10 10:29:34 +0900

    アイテム欄のもやアニメーション作成

--- CatEarsShota/Assets/Scripts/Horie/Items/ItemUIController.cs
+++ CatEarsShota/Assets/Scripts/Horie/Items/ItemUIController.cs
@@ -33,6 +33,9 @@ public class ItemUIController : MonoBehaviour
     [SerializeField]
     Camera FranCamera;
 
+    [SerializeField]
+    Animator[] moyas = new Animator[2];
+
 
     float AnimFrame = 8;
 
@@ -242,6 +245,8 @@ public class ItemUIController : MonoBehaviour
 
         if (!act) {
             PlayerPanel.Play("wait");
+            moyas[0].Play("Stop");
+            moyas[1].Play("Stop");
         }
 
         yield break;
@@ -310,6 +315,11 @@ public class ItemUIController : MonoBehaviour
         }
         else {
             PlayerPanel.Play("UI_Perrault_" + pantsNum);
+            if (FlagManager.Instance.CheckGimmickFlag(GimmickFlag_Map2.G_30_BlueAnimation))
+            {
+                moyas[0].Play("Play");
+                moyas[1].Play("Play");
+            }
         }
 
     }
@@ -499,6 +509,8 @@ public class ItemUIController : MonoBehaviour
         ItemPanel.GetComponent<RectTransform>().localPosition = new Vector3(0, -1080, 0);
         StopAllCoroutines();
         PlayerPanel.Play("wait");
+        moyas[0].Play("Stop");
+        moyas[1].Play("Stop");
         return true;
     }
 
@@ -528,6 +540,8 @@ public class ItemUIController : MonoBehaviour
                     IsEvents = false;
                     ItemPanel.GetComponent<RectTransform>().localPosition = new Vector3(0, -1080, 0);
                     PlayerPanel.Play("wait");
+                    moyas[0].Play("Stop");
+                    moyas[1].Play("Stop");
                     FlagManager.Instance.IsOpenUI = false;
                     callVoid();
                     yield break;
